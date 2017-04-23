226004aecdf948e109281c3fbcf0bc0db8e84217
vic3r <barcelonamane@hotmail.com>
2017-04-23 18:20:20 -0500

    changed id

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 const checkIfExists = require('../pageStatus/cassandraStatus');
 const Long = require('cassandra-driver').types.Long;
 const getFacebookId = require('../pageStatus/fbStatus');
+const go = require('node-go-require');
+const getCrawlerGo = require('../../../../../go/src/github.com/dtoledo23/influencer-detector-crawler/vendor/crawler/facebook/callAPIHelpers.go');
 
 // Define API routes.
 router.route("/page/status")
     .get(async(req, res) => {
-        let status = "";
+        let status = {};
 
         const url = req.query.url;
         if (!url) {
@@ -16,19 +18,21 @@ router.route("/page/status")
         let id = await getFacebookId(url)
         console.log("id", id)
         if (!id) {
-            res.send("non-existant");
+            res.json({ "id": null, "status": "non-existant" });
             return;
         }
 
         const idLong = Long.fromString(id);
         let fetched = await checkIfExists(idLong)
         if (fetched) {
-            status = "ready";
+            status = { "id": id, "status": "ready" };
         } else {
-            status = "fetchable";
+            status = { "id": id, "status": "fetchable" };
         }
 
-        res.send(status);
+        res.json(status);
     });
+
+
 
 module.exports = router;
